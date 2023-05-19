ae617a0fd8ca924f862241c01b3c04e102bb6ea1
Giovanni Mateus Barbieri <barbierig25@yahoo.com.br>
2023-05-19 18:39:13 -0300

    frontend is done!

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
     <div className='p-5 bg-primary flex justify-between text-4xl font-semibold'>
         <h1 className='text-secondary'>G</h1>
         <h1 className='text-white'>M</h1>
-        <h1 className='text-tertiary'>B</h1>
+        <h1 className='text-tertiary'>B</h1>  
     </div>
   )
 }
