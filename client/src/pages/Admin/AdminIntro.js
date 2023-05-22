8e23e5c9dd7dd89c4a6b6dfd59236291e3e2bace
Giovanni Mateus Barbieri <barbierig25@yahoo.com.br>
2023-05-22 19:23:32 -0300

    more admin development

--- client/src/pages/Admin/AdminIntro.js
+++ client/src/pages/Admin/AdminIntro.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Form } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { ShowLoading, HideLoading } from "../../redux/rootSlice";
+import axios from "axios";
+import { message } from "antd";
 
 function AdminIntro() {
+  const dispatch = useDispatch();
+  const { portfolioData } = useSelector((state) => state.root);
+
+  const onFinish = async (values) => {
+    try {
+      dispatch(ShowLoading());
+      const response = await axios.post("/api/portfolio/update-intro", {
+        ...values,
+        _id: portfolioData.intro._id,
+      });
+      dispatch(HideLoading());
+      if (response.data.success) {
+        message.success(response.data.message);
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      message.error(error.message);
+    }
+  };
+
   return (
-    <div>AdminIntro</div>
-  )
+    <div>
+      <Form
+        onFinish={onFinish}
+        layout="vertical"
+        initialValues={portfolioData.intro}
+      >
+        <Form.Item name="welcomeText" label="Welcome Text">
+          <input placeholder="Welcome Text" />
+        </Form.Item>
+        <Form.Item name="firstName" label="First Name">
+          <input placeholder="First Name" />
+        </Form.Item>
+        <Form.Item name="lastName" label="Last Name">
+          <input placeholder="Last Name" />
+        </Form.Item>
+        <Form.Item name="caption" label="Caption">
+          <input placeholder="Caption" />
+        </Form.Item>
+        <Form.Item name="description" label="Description">
+          <textarea placeholder="Description" />
+        </Form.Item>
+        <div className="flex justify-end w-full">
+          <button
+            className="px-10 py-2 bg-primary text-white rounded-md"
+            type="submit"
+          >
+            Save
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
 }
 
-export default AdminIntro
+export default AdminIntro;
--- client/src/pages/Admin/index.js
+++ client/src/pages/Admin/index.js
@@ -1,13 +1,39 @@
 import React from "react";
 import Header from "../../components/Header";
 import { Tabs } from "antd";
-const { TabPane } = Tabs;
+import AdminIntro from "./AdminIntro";
+import AdminAbout from "./AdminAbout";
+import { useSelector } from "react-redux";
+import AdminExperiences from "./AdminExperiences";
 
 function Admin() {
+  const { portfolioData } = useSelector((state) => state.root);
+  const tabs = [
+    {
+      label: "Intro",
+      key: "1",
+      children: <AdminIntro />,
+    },
+    {
+      label: "About",
+      key: "2",
+      children: <AdminAbout />,
+    },
+    {
+      label: "Experiences",
+      key: "3",
+      children: <AdminExperiences />,
+    },
+  ];
+
   return (
     <div>
       <Header />
-      <div></div>
+      {portfolioData && (
+        <div className="mt-5 p-5">
+          <Tabs defaultActiveKey="1" tabPosition="left" items={tabs}></Tabs>
+        </div>
+      )}
     </div>
   );
 }
