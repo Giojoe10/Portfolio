ede2a429584748f89ce8a9f42499b1d32e97746d
Giovanni <barbierig25@yahoo.com.br>
2023-05-22 14:44:37 -0300

    starting the admin page

---
+++ client/src/pages/Admin/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header";
+import { Tabs } from "antd";
+const { TabPane } = Tabs;
+
+function Admin() {
+  return (
+    <div>
+      <Header />
+      <div></div>
+    </div>
+  );
+}
+
+export default Admin;
