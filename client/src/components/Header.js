ce99f420ce9f885ffbfd8e5e34e08482c47c0f92
Giovanni Mateus Barbieri <barbierig25@yahoo.com.br>
2023-05-23 18:54:30 -0300

    even more development

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
-  return (
-    <div className='p-5 bg-primary flex justify-between text-4xl font-semibold'>
-        <h1 className='text-secondary'>G</h1>
-        <h1 className='text-white'>M</h1>
-        <h1 className='text-tertiary'>B</h1>  
-    </div>
-  )
+    return (
+        <div className="p-5 bg-primary   text-4xl font-semibold">
+            <a href="/" className="flex justify-between">
+                <h1 className="text-secondary">G</h1>
+                <h1 className="text-white">M</h1>
+                <h1 className="text-tertiary">B</h1>
+            </a>
+        </div>
+    );
 }
 
-export default Header
+export default Header;
